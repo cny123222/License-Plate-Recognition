3d5ddf60b42890fd5af19ed29004fed3b672fdc2
ephuon <clearlove2359@gmail.com>
2024-12-28 17:57:51 +0800

    Update ui.py

--- GUI/ui.py
+++ GUI/ui.py
@@ -1,15 +1,22 @@
 from tkinter.filedialog import *
-import random
 from tkinter import *
 from tkinter.ttk import *
 from PIL import Image, ImageTk
 from tkinter.font import Font
 
 class WinGUI(Tk):
+    # 初始化窗口 设置各个部件的位置 大小 颜色 字体 对应事件
+    # label用于显示图片&文字
+    # button用于响应用户的点击 运行函数
+    # frame用于放置部件 是label和button的载体
     def __init__(self):
         super().__init__()
         self.__win()
-        self.pic_path = ""
+        self.pyt = None # 原始图片 类型为ImageTk.PhotoImage
+        self.locate = None # 车牌定位图 类型为ImageTk.PhotoImage
+        self.color = "" # 车牌颜色 类型为str
+        self.result = "" # 识别结果 类型为str
+        self.pic_path = "" # 原图路径 类型为str
         self.tk_frame_frame_origin = self.__tk_frame_frame_origin(self)
         self.tk_label_label_origin = self.__tk_label_label_origin(self.tk_frame_frame_origin)
         self.tk_label_label_name = self.__tk_label_label_name(self)
@@ -23,10 +30,8 @@ class WinGUI(Tk):
         self.tk_label_label_result = self.__tk_label_label_result( self.tk_frame_frame_result) 
         self.tk_label_label_name_locate = self.__tk_label_label_name_locate(self)
         self.tk_label_label_name_result = self.__tk_label_label_name_result(self)
-        self.pyt = None
-        self.locate = None
-        self.color = None
-        self.result = ""
+
+        # 设置字体和字号
         font = Font(family="Arial", size=24, weight="bold")
         self.tk_label_label_name.configure(font=font)
         font = Font(family="STSong", size=15, weight="bold")
@@ -35,9 +40,10 @@ class WinGUI(Tk):
         self.tk_label_label_name_result.configure(font = font)
         font = Font(family="SimHei", size=30, weight="bold")
         self.tk_label_label_result.configure(font = font)
+
+    # 设置窗口大小 标题
     def __win(self):
         self.title("车牌识别系统")
-        # 设置窗口大小、居中
         width = 1000
         height = 700
         screenwidth = self.winfo_screenwidth()
@@ -46,44 +52,14 @@ class WinGUI(Tk):
         self.geometry(geometry)
         
         self.resizable(width=False, height=False)
-        
-    def scrollbar_autohide(self,vbar, hbar, widget):
-        """自动隐藏滚动条"""
-        def show():
-            if vbar: vbar.lift(widget)
-            if hbar: hbar.lift(widget)
-        def hide():
-            if vbar: vbar.lower(widget)
-            if hbar: hbar.lower(widget)
-        hide()
-        widget.bind("<Enter>", lambda e: show())
-        if vbar: vbar.bind("<Enter>", lambda e: show())
-        if vbar: vbar.bind("<Leave>", lambda e: hide())
-        if hbar: hbar.bind("<Enter>", lambda e: show())
-        if hbar: hbar.bind("<Leave>", lambda e: hide())
-        widget.bind("<Leave>", lambda e: hide())
-    
-    def v_scrollbar(self,vbar, widget, x, y, w, h, pw, ph):
-        widget.configure(yscrollcommand=vbar.set)
-        vbar.config(command=widget.yview)
-        vbar.place(relx=(w + x) / pw, rely=y / ph, relheight=h / ph, anchor='ne')
-    def h_scrollbar(self,hbar, widget, x, y, w, h, pw, ph):
-        widget.configure(xscrollcommand=hbar.set)
-        hbar.config(command=widget.xview)
-        hbar.place(relx=x / pw, rely=(y + h) / ph, relwidth=w / pw, anchor='sw')
-    def create_bar(self,master, widget,is_vbar,is_hbar, x, y, w, h, pw, ph):
-        vbar, hbar = None, None
-        if is_vbar:
-            vbar = Scrollbar(master)
-            self.v_scrollbar(vbar, widget, x, y, w, h, pw, ph)
-        if is_hbar:
-            hbar = Scrollbar(master, orient="horizontal")
-            self.h_scrollbar(hbar, widget, x, y, w, h, pw, ph)
-        self.scrollbar_autohide(vbar, hbar, widget)
+
+    # 连接识别函数 待处理……
     def connection(self, path):
         ##### to be changed #####
         # 返回路径, 识别结果, 颜色（RGB排列）
         return 'tst.jpg', '苏 CQ123222', '#FF0000'
+    
+    # “识别图片”按键对应的函数 作用是处理图片得到车牌并显示定位和结果
     def work(self):
         tmp, self.result, self.color = self.connection(self.pic_path)
         image = Image.open(tmp)
@@ -98,7 +74,6 @@ class WinGUI(Tk):
         # 缩放图片
         new_width = int(image_width * scale_ratio)
         new_height = int(image_height * scale_ratio)
-        print(new_width, new_height)    
 
         image = image.resize((new_width, new_height), Image.LANCZOS)
         # 将缩放后的图片转换为PhotoImage对象
@@ -106,6 +81,7 @@ class WinGUI(Tk):
         self.tk_label_label_result.configure(text=self.result, background=self.color)
         self.tk_label_label_locate.configure(image=self.locate)
 
+    # “选择图片”按键对应的函数 作用是选择图片并显示原图
     def choose_pic(self):
         self.pic_path = askopenfilename(title="选择识别图片", filetypes=[("jpg图片", "*.jpg"),("png图片", "*.png")])
         if self.pic_path:
@@ -126,71 +102,72 @@ class WinGUI(Tk):
             self.pyt = ImageTk.PhotoImage(image)
             # 配置Label的image属性
             self.tk_label_label_origin.configure(image=self.pyt)
+    # 初始化装载原图的frame
     def __tk_frame_frame_origin(self,parent):
         frame = Frame(parent, borderwidth=5, relief='ridge')
         frame.place(x=35, y=175, width=690, height=455)
         return frame    
+    # 初始化显示原图的label
     def __tk_label_label_origin(self,parent):
         label = Label(parent,anchor="center", )
         label.place(width=680, height=445)
         return label
+    # 初始化label用于显示标题
     def __tk_label_label_name(self,parent):
         label = Label(parent,text="车牌识别系统",anchor="center", )
         label.place(x=315, y=65, width=370, height=74)
         return label
+    # 初始化frame用于装载命令栏
     def __tk_frame_frame_command(self,parent):
         frame = Frame(parent, borderwidth=5, relief='ridge')
         frame.place(x=740, y=175, width=242, height=144)
         return frame
+    # 初始化label用于显示文字 “命令：”
     def __tk_label_label_name_command(self,parent):
         label = Label(parent,text="命令：",anchor="w", )
         label.place(x=0, y=7, width=77, height=30)
         return label
+    # 初始化button用于选择图片
     def __tk_button_button_choose(self,parent):
         btn = Button(parent, command=self.choose_pic, text="选择图片", takefocus=False)
         btn.place(x=46, y=48, width=150, height=30)
         return btn
+    # 初始化button用于识别图片
     def __tk_button_button_apply(self,parent):
         btn = Button(parent, command=self.work, text="识别图片", takefocus=False,)
         btn.place(x=46, y=93, width=150, height=30)
         return btn
+    # 初始化frame用于装载车牌定位图
     def __tk_frame_frame_locate(self,parent):
         frame = Frame(parent, borderwidth=5, relief='ridge')
         frame.place(x=740, y=372, width=242, height=100)
         return frame
+    # 初始化label用于显示车牌定位图
     def __tk_label_label_locate(self,parent):
         label = Label(parent,anchor="center", )
         label.place(x=0, y=0, width=235, height=93)
         return label
+    # 初始化frame用于装载车牌识别结果
     def __tk_frame_frame_result(self,parent):
         frame = Frame(parent, borderwidth=5, relief='ridge')
         frame.place(x=740, y=530, width=242, height=100)
         return frame
+    # 初始化label用于显示车牌识别结果
     def __tk_label_label_result(self,parent):
         label = Label(parent,anchor="center")
         label.place(x=0, y=0, width=235, height=93)
         return label
+    # 初始化label用于显示文字 “车牌定位：”
     def __tk_label_label_name_locate(self,parent):
         label = Label(parent,text="车牌定位：",anchor="center", )
         label.place(x=740, y=332, width=100, height=30)
         return label
+    # 初始化label用于显示文字 “预测结果：”
     def __tk_label_label_name_result(self,parent):
         label = Label(parent,text="预测结果：",anchor="center", )
         label.place(x=740, y=490, width=100, height=30)
         return label
 
-
-class Win(WinGUI):
-    def __init__(self, controller):
-        self.ctl = controller
-        super().__init__()
-        self.__event_bind()
-        self.__style_config()
-        self.ctl.init(self)
-    def __event_bind(self):
-        pass
-    def __style_config(self):
-        pass
 if __name__ == "__main__":
-    win = WinGUI()
-    win.mainloop()
+    win = WinGUI() # 创建窗口
+    win.mainloop() # 开始循环（等待用户操作）
