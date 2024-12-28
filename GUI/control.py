ef76f389af73a271ab86ebcd9c9406ee6354761e
ephuon <clearlove2359@gmail.com>
2024-12-28 17:39:29 +0800

    Update control.py

--- GUI/control.py
+++ GUI/control.py
@@ -1,10 +1,3 @@
-"""
-本代码由[Tkinter布局助手]生成
-官网:https://www.pytk.net
-QQ交流群:905019785
-在线反馈:https://support.qq.com/product/618914
-"""
-# 示例下载 https://www.pytk.net/blog/1702564569.html
 class Controller:
     # 导入UI类后，替换以下的 object 类型，将获得 IDE 属性提示功能
     ui: object
